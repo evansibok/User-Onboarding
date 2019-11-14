73e48c2fea52dc2567c0dd764d08258e1a2a19df
Evans Ibok <evansibok@gmail.com>
2019-11-14 14:07:44 +0100

    completed the UserForm component

--- user-onboard/src/components/UserForm.js
+++ user-onboard/src/components/UserForm.js
@@ -2,7 +2,37 @@ import React from "react";
 import { withFormik, Form, Field, ErrorMessage } from "formik";
 
 const UserForm = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Form>
+        <label>
+          Name:
+          <Field type="text" name="name" />
+        </label>
+        <br />
+        <br />
+        <label>
+          Email:
+          <Field type="email" name="email" />
+        </label>
+        <br />
+        <br />
+        <label>
+          Password:
+          <Field type="password" name="password" />
+        </label>
+        <br />
+        <br />
+        <label>
+          I accept the terms and service!
+          <Field type="checkbox" name="terms" />
+        </label>
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </Form>
+    </div>
+  );
 };
 
 const UserFormikForm = withFormik({
